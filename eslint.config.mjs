0447273cd09d19b0c0d2cd0621a19d65596b014e
Triston Land <tristonland@gmail.com>
2025-02-07 05:09:40 -0700

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
 ];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./styles.css";
 
 console.log("hello world");
+
+let yo = "too";
+
+let hi
